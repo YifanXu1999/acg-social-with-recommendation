8ac7ce49f2ddd99bd689fd07945d5ce916e490b7
YifanXu1999 <yifanxu1999@gmail.com>
2024-11-07 00:23:22 -0500

    readme progress tracker

--- backend/headnews-java/headnews-services/headnews-user-service/src/main/java/com/yifan/userserice/controller/HelloWorld.java
+++ backend/headnews-java/headnews-services/headnews-user-service/src/main/java/com/yifan/userserice/controller/HelloWorld.java
@@ -13,6 +13,6 @@ public class HelloWorld {
     private String dataMessage;
     @GetMapping
     public String hello() {
-        return dataMessage;
+        return dataMessage + " Hello World!";
     }
 }
