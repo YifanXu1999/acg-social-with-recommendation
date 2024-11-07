83f69d35f166eb91383922881724b14d2d58d37f
YifanXu1999 <yifanxu1999@gmail.com>
2024-11-07 00:03:01 -0500

    Consul kv store setup

---
+++ backend/headnews-java/headnews-services/headnews-user-service/src/main/java/com/yifan/userserice/controller/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.yifan.userserice.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorld {
+
+    @Value("${dataMessage}")
+    private String dataMessage;
+    @GetMapping
+    public String hello() {
+        return dataMessage;
+    }
+}
