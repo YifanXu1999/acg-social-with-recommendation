bb3096a7e893a7de5a1487120d535ff3e31de5d6
YifanXu1999 <yifanxu1999@gmail.com>
2024-11-09 12:35:59 -0500

    Reorganize code

--- backend/headnews-java/headnews-gateways/headnews-user-gateway/src/main/java/com/yifan/user/gateway/UserGatewayApplication.java
+++ backend/headnews-java/headnews-gateways/headnews-user-gateway/src/main/java/com/yifan/user/gateway/UserGatewayApplication.java
@@ -3,16 +3,14 @@ package com.yifan.user.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
+
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class UserGatewayApplication {
+
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(UserGatewayApplication.class, args);
 
