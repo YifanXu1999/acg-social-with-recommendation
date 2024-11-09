aa9334b9608d8b57f127b15eea02435ce072329a
YifanXu1999 <yifanxu1999@gmail.com>
2024-11-08 20:31:54 -0500

    Update user-service security whitelist

--- backend/headnews-java/headnews-services/headnews-user-service/src/main/java/com/yifan/user/service/configuration/SecurityConfig.java
+++ backend/headnews-java/headnews-services/headnews-user-service/src/main/java/com/yifan/user/service/configuration/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((authorize) -> {
-                    authorize.requestMatchers("/login").permitAll();
+                    authorize.requestMatchers("/login", "/actuator/**").permitAll();
                     authorize.anyRequest().authenticated();
                 }).httpBasic(Customizer.withDefaults());
 
