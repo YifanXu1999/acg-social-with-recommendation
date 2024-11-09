56ec1ee60bf5a2edef6915bf7605d8789dd401e5
YifanXu1999 <yifanxu1999@gmail.com>
2024-11-09 16:30:17 -0500

    Init models module

---
+++ backend/headnews-java/headnews-models/src/main/java/com/yifan/models/HeadNewsDataApplication.java
@@ -0,0 +1,12 @@
+package com.yifan.models;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadNewsDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeadNewsDataApplication.class, args);
+    }
+}
---
+++ backend/headnews-java/headnews-models/src/main/java/com/yifan/models/pojo/user/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.yifan.models.pojo.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@MappedSuperclass
+@Getter
+@Setter
+public abstract class BaseEntity {
+
+    @Column(updatable = false)
+    private Date createdAt;
+
+    @Column
+    private Date updatedAt;
+
+}
