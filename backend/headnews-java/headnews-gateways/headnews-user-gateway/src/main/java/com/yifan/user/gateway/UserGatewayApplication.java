5594a73944d0c795c376230e6e800368a5608c0b
YifanXu1999 <yifanxu1999@gmail.com>
2024-11-09 13:24:09 -0500

    Enable componentscan for usergateway

--- backend/headnews-java/headnews-gateways/headnews-user-gateway/src/main/java/com/yifan/user/gateway/UserGatewayApplication.java
+++ backend/headnews-java/headnews-gateways/headnews-user-gateway/src/main/java/com/yifan/user/gateway/UserGatewayApplication.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
-
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan(basePackages = "com.yifan")
 public class UserGatewayApplication {
 
     public static void main(String[] args) {
