4716147b98da3a263212decdaaeb61d0f555b04c
YifanXu1999 <yifanxu1999@gmail.com>
2024-11-09 13:04:11 -0500

    Add custom exception handler

---
+++ backend/headnews-java/headnews-commons/src/main/java/com/yifan/common/exception/CustomException.java
@@ -0,0 +1,17 @@
+package com.yifan.common.exception;
+
+
+import com.yifan.common.enum_.AppHttpCodeEnum;
+
+public class CustomException extends RuntimeException {
+
+    private AppHttpCodeEnum appHttpCodeEnum;
+
+    public CustomException(AppHttpCodeEnum appHttpCodeEnum){
+        this.appHttpCodeEnum = appHttpCodeEnum;
+    }
+
+    public AppHttpCodeEnum getAppHttpCodeEnum() {
+        return appHttpCodeEnum;
+    }
+}
---
+++ backend/headnews-java/headnews-commons/src/main/java/com/yifan/common/exception/ExceptionCatch.java
@@ -0,0 +1,32 @@
+package com.yifan.common.exception;
+
+import com.yifan.common.enum_.AppHttpCodeEnum;
+import com.yifan.common.result.ResponseResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+@Component("customControllerAdvice")
+@Slf4j
+public class ExceptionCatch {
+
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public ResponseResult exception(Exception e){
+        e.printStackTrace();
+        log.error("catch exception:{}",e.getMessage());
+        return ResponseResult.errorResult(AppHttpCodeEnum.SERVER_ERROR);
+    }
+
+
+    @ExceptionHandler(CustomException.class)
+    @ResponseBody
+    public ResponseResult exception(CustomException e){
+        log.error("catch exception:{}",e);
+        return ResponseResult.errorResult(e.getAppHttpCodeEnum());
+    }
+}
