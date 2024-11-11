333efeeaf061db0662f61067be52f4242fcb5548
YifanXu1999 <yifanxu1999@gmail.com>
2024-11-10 19:27:13 -0500

    header

---
+++ frontend/acg-social-nextjs/src/component/dashboard/header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function DashboardHeader() {
+  return (
+    <header className="fixed h-32 bg-amber-500">
+      <div className="flex justify-between items-center">
+        <div className="">
+          ACG Social
+        </div>
+      </div>
+    </header>
+  )
+}
