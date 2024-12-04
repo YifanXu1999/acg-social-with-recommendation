311fca2f5b5687fef79fed45f433dac85046768b
YifanXu1999 <yifanxu1999@gmail.com>
2024-12-03 23:36:00 -0500

    gateway webflux configuration

--- backend/acg-social-java/acg-social-gateways/acg-social-user-gateway/src/main/java/com/acgsocial/user/gateway/route/RouteConfig.java
+++ backend/acg-social-java/acg-social-gateways/acg-social-user-gateway/src/main/java/com/acgsocial/user/gateway/route/RouteConfig.java
@@ -18,31 +18,18 @@ public class RouteConfig {
 
     @Bean
     @LoadBalanced
-    public WebClient loadBalancedWebClient() {
-        /********
-         * Working Case
-         * reactor.core.Exceptions$ErrorCallbackNotImplemented: org.springframework.web.reactive.function.client.WebClientRequestException: Failed to resolve 'user-service' [A(1)] after 2 queries
-         * Caused by: org.springframework.web.reactive.function.client.WebClientRequestException: Failed to resolve 'user-service' [A(1)] after 2 queries
-         * 	at org.springframework.web.reactive.function.client.ExchangeFunctions$DefaultExchangeFunction.lambda$wrapException$9(ExchangeFunctions.java:137) ~[spring-webflux-6.2.0.jar:6.2.0]
-         * 	Suppressed: reactor.core.publisher.FluxOnAssembly$OnAssemblyException:
-         * Error has been observed at the following site(s):
-         * 	*__checkpoint ⇢ Request to GET http://user-service/hello/a [DefaultWebClient]
-         *.................
-         * ..................
-         * ..................
-         * Caused by: io.netty.resolver.dns.DnsErrorCauseException: Query failed with NXDOMAIN
-         * 	at io.netty.resolver.dns.DnsResolveContext.onResponse(..)(Unknown Source) ~[netty-resolver-dns-4.1.115.Final.jar:4.1.115.Final]
-         */
-        return WebClient.builder().build();
+    public WebClient.Builder loadBalancedWebClient() {
+
+        return WebClient.builder();
         /********/
     }
 
 
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder, RedissonClient redisson,
-                                           WebClient loadBalancedWebClient) {
+                                           WebClient.Builder loadBalancedWebClient) {
 
-        WebClient webClient = loadBalancedWebClient;
+        WebClient webClient = loadBalancedWebClient.build();
         return builder.routes()
                 .route("get_route", r -> r.path("/user-service/**")
                         .filters(
